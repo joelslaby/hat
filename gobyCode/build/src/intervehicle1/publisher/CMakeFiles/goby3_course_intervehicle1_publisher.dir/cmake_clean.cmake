d8f2df5460f5b4a0a9ac190ca22ad1b39fd550ba
James Ellis <jamo1011@gmail.com>
2021-03-17 17:17:23 +0000

    intervehicle communications over UDP

---
+++ gobyCode/build/src/intervehicle1/publisher/CMakeFiles/goby3_course_intervehicle1_publisher.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "../../../bin/goby3_course_intervehicle1_publisher"
+  "../../../bin/goby3_course_intervehicle1_publisher.pdb"
+  "CMakeFiles/goby3_course_intervehicle1_publisher.dir/app.cpp.o"
+  "CMakeFiles/goby3_course_intervehicle1_publisher.dir/config.pb.cc.o"
+  "config.pb.cc"
+  "config.pb.h"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/goby3_course_intervehicle1_publisher.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
