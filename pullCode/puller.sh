3782611e109200cdc74a89033b0960fc96cd6f02
James Fan <jamescasdf@gmail.com>
2021-03-19 13:19:02 -0400

    committing code to pull GPS data

---
+++ pullCode/puller.sh
@@ -0,0 +1,20 @@
+# .sh file
+#
+#!/bin/bash
+#
+# steps to follow
+# ACCESS PI
+cd testCD
+#ftp 192.168.1.11
+#echo "pi"
+#echo "raspberry"
+#cd Documents
+#cd ftp
+#get test.txt
+#exit
+#
+# MOVE TO DESIRED DIRECTORY
+#mv test.txt testMove/
+#
+# NOTE: MOVING FILE TO DIRECTORY CONTAINING FILE W/SAME NAME
+# WILL OVERWRITE
---
+++ pullCode/puller_execute.c
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+
+int main()
+{
+	system("./puller.sh");
+	return 0;
+}
