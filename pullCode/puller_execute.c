39e6cbec29143568684c7799f55a5653e8387f77
James Fan <jamescasdf@gmail.com>
2021-03-26 14:27:18 -0400

    modified puller c code

--- pullCode/puller_execute.c
+++ pullCode/puller_execute.c
@@ -1,7 +1,55 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <iostream>
+
+using namespace std;
+
+void delay(int number_of_seconds);
 
 int main()
 {
-	system("./puller.sh");
+	cout << "program start\n";
+	while(1)
+	{
+		cout << "pulling files:\n";
+		system("./puller.sh"); //NOTE: program does not continue until script is done and files are pulled
+		cout << "files pulled\n";
+
+		FILE *fp1 = fopen("test1.txt", "r'");
+		FILE *fp2 = fopen("test2.txt", "r'");
+		FILE *fp3 = fopen("test3.txt", "r'");
+
+		FILE *fp4 = fopen("tester.txt", "w");
+		char c;
+
+		while((c = fgetc(fp1)) != EOF)
+			fputc(c,fp4);
+			cout << "copied file 1\n";
+		while((c = fgetc(fp2)) != EOF)
+			fputc(c,fp4);
+			cout << "copied file 2\n";
+		while((c = fgetc(fp3)) != EOF)
+			fputc(c,fp4);
+			cout << "copied file 3\n";
+
+		fclose(fp1); fclose(fp2); fclose(fp3); fclose(fp4);
+
+		delay(2);
+		cout << "restarting loop:\n";
+	}
 	return 0;
+}
+
+void delay(int number_of_seconds)
+{
+    // Converting time into milli_seconds
+    int ticks = 1000000 * number_of_seconds; //multiplier based off of clock ticks, not milliseconds; aka 1 million ticks per second on 32 bit
+  
+    // Storing start time in terms of clock ticks
+    clock_t start_t = clock();
+  
+    // looping till required time is not achieved
+    while (clock() < start_t + ticks)
+		;
 }
