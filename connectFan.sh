6e045978cc010775e996e16eed0720b9c10a5771
mmuqsit3 <72168520+mmuqsit3@users.noreply.github.com>
2021-04-08 17:48:50 -0400

    Added connectJoel and connectFan files

---
+++ connectFan.sh
@@ -0,0 +1,5 @@
+cd 
+cd /etc/netplan
+cp networks/fan_router.yaml networks/50-cloud-init.yaml
+rm 50-cloud-init.yaml
+mv networks/50-cloud-init.yaml 50-cloud-init.yaml
---
+++ connectJoel.sh
@@ -0,0 +1,5 @@
+cd 
+cd /etc/netplan
+cp networks/js_hotspot.yaml networks/50-cloud-init.yaml
+rm 50-cloud-init.yaml
+mv networks/50-cloud-init.yaml 50-cloud-init.yaml
