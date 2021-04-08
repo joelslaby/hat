49674cd8194d74f7cdc47d86f9eaecef519c3d05
James <jamo1011@gmail.com>
2021-04-08 16:04:01 -0400

    added comments

--- gobyCode/src/receiver/app.cpp
+++ gobyCode/src/receiver/app.cpp
@@ -37,13 +37,15 @@ hat::apps::Receiver::Receiver()
 
     file_out.open(filename, std::ios_base::out);
     
+    //runs when a transmission is received
     auto on_distress_signal = [this](const hat::protobuf::DistressSignal& distress_signal_msg) {
         glog.is_verbose() && glog << "Received Signal: " << distress_signal_msg.ShortDebugString() << std::endl;
 
-	// Insert what we should do with the message
+	// writes received message under current time to a file
 	file_out << distress_signal_msg.ShortDebugString() << " time: " << time(NULL) << std::endl;
     };
 
+    // subscribes to DistressSignal messages 
     goby::middleware::protobuf::TransporterConfig subscriber_cfg;
     subscriber_cfg.mutable_intervehicle()->add_publisher_id(1);
     goby::middleware::Subscriber<hat::protobuf::DistressSignal> distress_signal_subscriber(subscriber_cfg);
