8cf0bdb47ec5360739a82f5e438e2dff2baf493b
joelslaby <joelslaby@gatech.edu>
2021-04-09 14:05:59 +0000

    fixed connect files

--- connectJoel.sh
+++ connectJoel.sh
@@ -3,3 +3,5 @@ cd /etc/netplan
 cp networks/js_hotspot.yaml networks/50-cloud-init.yaml
 rm 50-cloud-init.yaml
 mv networks/50-cloud-init.yaml 50-cloud-init.yaml
+sudo netplan apply
+sudo netplan generate
